31392f447f671e00b6cfbe651810b9d1a049323f
Joe Schoonover <joe@fluidnumerics.com>
2021-07-26 20:51:36 +0000

    Re-add create-a-globally-scalable-partition

--- update-codelabs.sh
+++ update-codelabs.sh
@@ -2,15 +2,17 @@
 
 
 while read l; do
+
         app=$(echo $l | awk -F " : " '{print $1}')
         subdir=$(echo $l | awk -F " : " '{print $2}')
-	echo $subdir
         docid=$(echo $l | awk -F " : " '{print $3}')
 	claat export $docid
+
 	mkdir -p $app
-	if [ -d "$app/$path" ]; then
-		rm -rf $app/$path
+	if [ -d "$app/$subdir" ]; then
+		rm -rf $app/$subdir
 	fi
 	mkdir -p $app/$subdir
 	mv $subdir $app/$subdir
+
 done <docs.txt 
