0c2cd75a9193abea027bea2dd9c29179bb40168c
Joe Schoonover <joe@fluidnumerics.com>
2022-02-02 12:48:47 -0700

    [PUB-538] Migrate existing codelabs into this repository

---
+++ install-claat.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+wget https://github.com/googlecodelabs/tools/releases/download/v2.2.4/claat-linux-amd64
+mv claat-linux-amd64 ./bin/claat
+chmod +x ./bin/claat
--- update-codelabs.sh
+++ update-codelabs.sh
@@ -4,15 +4,7 @@
 while read l; do
 
         app=$(echo $l | awk -F " : " '{print $1}')
-        subdir=$(echo $l | awk -F " : " '{print $2}')
-        docid=$(echo $l | awk -F " : " '{print $3}')
-	claat export $docid
-
-	mkdir -p $app
-	if [ -d "$app/$subdir" ]; then
-		rm -rf $app/$subdir
-	fi
-	#mkdir -p $app/$subdir
-	mv $subdir/ $app/
+        docid=$(echo $l | awk -F " : " '{print $2}')
+	./bin/claat export $docid
 
 done <docs.txt 
